077dfa596dbcd0d673dbc4a9825e9007e708d037
rleungx <rleungx@gmail.com>
2019-01-24 14:36:47 +0800

    add the benchmark
    Signed-off-by: rleungx <rleungx@gmail.com>

--- server/cluster_test.go
+++ server/cluster_test.go
@@ -575,3 +575,28 @@ func (s *testClusterSuite) TestConcurrentHandleRegion(c *C) {
 	}
 	wg.Wait()
 }
+
+var _ = Suite(&testGetStoresSuite{})
+
+type testGetStoresSuite struct {
+	cluster *clusterInfo
+}
+
+func (s *testGetStoresSuite) SetUpSuite(c *C) {
+	_, opt, err := newTestScheduleConfig()
+	c.Assert(err, IsNil)
+	s.cluster = newClusterInfo(core.NewMockIDAllocator(), opt, core.NewKV(core.NewMemoryKV()))
+
+	stores := newTestStores(200)
+
+	for _, store := range stores {
+		c.Assert(s.cluster.putStore(store), IsNil)
+	}
+}
+
+func (s *testGetStoresSuite) BenchmarkGetStores(c *C) {
+	for i := 0; i < c.N; i++ {
+		// Logic to benchmark
+		s.cluster.core.Stores.GetStores()
+	}
+}
