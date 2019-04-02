1c2a2da7532788dbeacb1c192a1ce21b03ea372f
Ryan Leung <rleungx@gmail.com>
2019-04-02 17:12:57 +0800

    set MaxScheduleCost to 0 by default
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/schedule/operator_test.go
+++ server/schedule/operator_test.go
@@ -123,7 +123,7 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: 0,
 		RegionSize:  10,
 		RegionCount: 1,
-		StepCost:    2,
+		StepCost:    10,
 	})
 
 	TransferLeader{FromStore: 1, ToStore: 2}.Influence(opInfluence, region)
@@ -139,7 +139,7 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: 1,
 		RegionSize:  10,
 		RegionCount: 1,
-		StepCost:    3,
+		StepCost:    11,
 	})
 
 	RemovePeer{FromStore: 1}.Influence(opInfluence, region)
@@ -148,14 +148,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  -10,
 		RegionCount: -1,
-		StepCost:    3,
+		StepCost:    11,
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  10,
 		LeaderCount: 1,
 		RegionSize:  10,
 		RegionCount: 1,
-		StepCost:    3,
+		StepCost:    11,
 	})
 
 	MergeRegion{IsPassive: false}.Influence(opInfluence, region)
@@ -164,14 +164,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  -10,
 		RegionCount: -1,
-		StepCost:    3,
+		StepCost:    11,
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  10,
 		LeaderCount: 1,
 		RegionSize:  10,
 		RegionCount: 1,
-		StepCost:    3,
+		StepCost:    11,
 	})
 
 	MergeRegion{IsPassive: true}.Influence(opInfluence, region)
@@ -180,14 +180,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -2,
 		RegionSize:  -10,
 		RegionCount: -2,
-		StepCost:    6,
+		StepCost:    22,
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  10,
 		LeaderCount: 1,
 		RegionSize:  10,
 		RegionCount: 0,
-		StepCost:    5,
+		StepCost:    21,
 	})
 }
 
