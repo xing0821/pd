f01f45d357bf8b1723875aab333de4cdac66191b
Ryan Leung <rleungx@gmail.com>
2019-07-04 15:59:11 +0800

    fix race problems
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/schedule/operator.go
+++ server/schedule/operator.go
@@ -39,7 +39,8 @@ const (
 	RegionOperatorWaitTime = 10 * time.Minute
 	// RegionInfluence represents the influence of a operator step, which is used by ratelimit.
 	RegionInfluence int64 = 1000
-	// smallRegionInfluence represents the influence of a operator step, which is used by ratelimit.
+	// smallRegionInfluence represents the influence of a operator step
+	// when the region size is smaller than smallRegionThreshold, which is used by ratelimit.
 	smallRegionInfluence int64 = 200
 	// smallRegionThreshold is used to represent a region which can be regarded as a small region once the size is small than it.
 	smallRegionThreshold int64 = 20
