3689ef86eadaf4d93b283376e9f1665f343569c2
Ryan Leung <rleungx@gmail.com>
2019-08-15 21:33:29 +0800

    use a checker controller
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

---
+++ server/schedule/checker_controller.go
@@ -0,0 +1,81 @@
+package schedule
+
+import (
+	"github.com/pingcap/pd/server/core"
+	"github.com/pingcap/pd/server/namespace"
+	"github.com/pingcap/pd/server/schedule/checker"
+	"github.com/pingcap/pd/server/schedule/operator"
+	"github.com/pingcap/pd/server/schedule/opt"
+)
+
+// CheckerController is used to manage all checkers.
+type CheckerController struct {
+	cluster          opt.Cluster
+	opController     *OperatorController
+	learnerChecker   *checker.LearnerChecker
+	replicaChecker   *checker.ReplicaChecker
+	namespaceChecker *checker.NamespaceChecker
+	mergeChecker     *checker.MergeChecker
+}
+
+// NewCheckerController create a new CheckerController.
+// TODO: isSupportMerge should be removed.
+func NewCheckerController(cluster opt.Cluster, classifier namespace.Classifier, opController *OperatorController, isSupportMerge bool) *CheckerController { // revive:disable-line:flag-parameter
+	cc := &CheckerController{
+		cluster:          cluster,
+		opController:     opController,
+		learnerChecker:   checker.NewLearnerChecker(),
+		replicaChecker:   checker.NewReplicaChecker(cluster, classifier),
+		namespaceChecker: checker.NewNamespaceChecker(cluster, classifier),
+	}
+	if isSupportMerge {
+		cc.mergeChecker = checker.NewMergeChecker(cluster, classifier)
+	}
+
+	return cc
+}
+
+// CheckRegion will check the region and add a new operator if needed.
+func (c *CheckerController) CheckRegion(region *core.RegionInfo) bool {
+	// If PD has restarted, it need to check learners added before and promote them.
+	// Don't check isRaftLearnerEnabled cause it maybe disable learner feature but there are still some learners to promote.
+	opController := c.opController
+
+	if op := c.learnerChecker.Check(region); op != nil {
+		if opController.AddOperator(op) {
+			return true
+		}
+	}
+
+	if opController.OperatorCount(operator.OpLeader) < c.cluster.GetLeaderScheduleLimit() &&
+		opController.OperatorCount(operator.OpRegion) < c.cluster.GetRegionScheduleLimit() &&
+		opController.OperatorCount(operator.OpReplica) < c.cluster.GetReplicaScheduleLimit() {
+		if op := c.namespaceChecker.Check(region); op != nil {
+			if opController.AddWaitingOperator(op) {
+				return true
+			}
+		}
+	}
+
+	if opController.OperatorCount(operator.OpReplica) < c.cluster.GetReplicaScheduleLimit() {
+		if op := c.replicaChecker.Check(region); op != nil {
+			if opController.AddWaitingOperator(op) {
+				return true
+			}
+		}
+	}
+	if c.mergeChecker != nil && opController.OperatorCount(operator.OpMerge) < c.cluster.GetMergeScheduleLimit() {
+		if ops := c.mergeChecker.Check(region); ops != nil {
+			// It makes sure that two operators can be added successfully altogether.
+			if opController.AddWaitingOperator(ops...) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// GetMergeChecker returns the merge checker.
+func (c *CheckerController) GetMergeChecker() *checker.MergeChecker {
+	return c.mergeChecker
+}
