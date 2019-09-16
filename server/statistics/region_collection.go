ab56088c26fe4e29165e89205ee870f195339315
Ryan Leung <rleungx@gmail.com>
2019-09-16 20:45:32 +0800

    address the comment
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/statistics/region_collection.go
+++ server/statistics/region_collection.go
@@ -248,12 +248,3 @@ func notIsolatedStoresWithLabel(stores []*core.StoreInfo, label string) [][]*cor
 	}
 	return res
 }
-
-func contains(s []int, e int) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
