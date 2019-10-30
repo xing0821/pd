0b2da0191b2820d656ac9580186dab1a769c87dd
Ryan Leung <rleungx@gmail.com>
2019-10-30 11:19:34 +0800

    change to millisecond
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/grpc_service.go
+++ server/grpc_service.go
@@ -89,7 +89,7 @@ func (s *Server) Tso(stream pdpb.PD_TsoServer) error {
 		}
 
 		elapsed := time.Since(start)
-		if elapsed > time.Second {
+		if elapsed > time.Millisecond {
 			log.Warn("get timestamp too slow", zap.Duration("cost", elapsed))
 		}
 		response := &pdpb.TsoResponse{
