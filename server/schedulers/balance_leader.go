pingcap/tidb-dashboard

schedule: add more dimension for filter metrics (#1746)
Signed-off-by: Ryan Leung <rleungx@gmail.com>

server/schedulers/balance_leader.go
@@ -49,15 +49,10 @@ type balanceLeaderScheduler struct {
 // each store balanced.
 func newBalanceLeaderScheduler(opController *schedule.OperatorController, opts ...BalanceLeaderCreateOption) schedule.Scheduler {
 	taintStores := newTaintCache()
-	filters := []filter.Filter{
-		filter.StoreStateFilter{TransferLeader: true},
-		filter.NewCacheFilter(taintStores),
-	}
 	base := newBaseScheduler(opController)
 
 	s := &balanceLeaderScheduler{
 		baseScheduler: base,
-		selector:      selector.NewBalanceSelector(core.LeaderKind, filters),
 		taintStores:   taintStores,
 		opController:  opController,
 		counter:       balanceLeaderCounter,
@@ -65,6 +60,11 @@ func newBalanceLeaderScheduler(opController *schedule.OperatorController, opts .
 	for _, opt := range opts {
 		opt(s)
 	}
+	filters := []filter.Filter{
+		filter.StoreStateFilter{ActionScope: s.GetName(), TransferLeader: true},
+		filter.NewCacheFilter(s.GetName(), taintStores),
+	}
+	s.selector = selector.NewBalanceSelector(core.LeaderKind, filters)
 	return s
 }
 
@@ -228,7 +228,7 @@ func (l *balanceLeaderScheduler) createOperator(cluster schedule.Cluster, region
 	targetLabel := strconv.FormatUint(targetID, 10)
 	l.counter.WithLabelValues("move-leader", source.GetAddress()+"-out", sourceLabel).Inc()
 	l.counter.WithLabelValues("move-leader", target.GetAddress()+"-in", targetLabel).Inc()
-	balanceDirectionCounter.WithLabelValues(l.GetName(), sourceLabel, targetLabel)
+	balanceDirectionCounter.WithLabelValues(l.GetName(), sourceLabel, targetLabel).Inc()
 	op := operator.CreateTransferLeaderOperator("balance-leader", region, region.GetLeader().GetStoreId(), targetID, operator.OpBalance)
 	return []*operator.Operator{op}
 }

server/schedulers/balance_region.go
@@ -62,13 +62,9 @@ type balanceRegionScheduler struct {
 // newBalanceRegionScheduler creates a scheduler that tends to keep regions on
 // each store balanced.
 func newBalanceRegionScheduler(opController *schedule.OperatorController, opts ...BalanceRegionCreateOption) schedule.Scheduler {
-	filters := []filter.Filter{
-		filter.StoreStateFilter{MoveRegion: true},
-	}
 	base := newBaseScheduler(opController)
 	s := &balanceRegionScheduler{
 		baseScheduler: base,
-		selector:      selector.NewBalanceSelector(core.RegionKind, filters),
 		opController:  opController,
 		hitsCounter:   newHitsStoreBuilder(hitsStoreTTL, hitsStoreCountThreshold),
 		counter:       balanceRegionCounter,
@@ -76,6 +72,10 @@ func newBalanceRegionScheduler(opController *schedule.OperatorController, opts .
 	for _, opt := range opts {
 		opt(s)
 	}
+	filters := []filter.Filter{
+		filter.StoreStateFilter{ActionScope: s.GetName(), MoveRegion: true},
+	}
+	s.selector = selector.NewBalanceSelector(core.RegionKind, filters)
 	return s
 }
 
@@ -116,7 +116,7 @@ func (s *balanceRegionScheduler) Schedule(cluster schedule.Cluster) []*operator.
 	stores := cluster.GetStores()
 
 	// source is the store with highest region score in the list that can be selected as balance source.
-	f := s.hitsCounter.buildSourceFilter(cluster)
+	f := s.hitsCounter.buildSourceFilter(s.GetName(), cluster)
 	source := s.selector.SelectSource(cluster, stores, f)
 	if source == nil {
 		schedulerCounter.WithLabelValues(s.GetName(), "no-store").Inc()
@@ -185,9 +185,9 @@ func (s *balanceRegionScheduler) transferPeer(cluster schedule.Cluster, region *
 	if source == nil {
 		log.Error("failed to get the source store", zap.Uint64("store-id", sourceStoreID))
 	}
-	scoreGuard := filter.NewDistinctScoreFilter(cluster.GetLocationLabels(), stores, source)
-	hitsFilter := s.hitsCounter.buildTargetFilter(cluster, source)
-	checker := checker.NewReplicaChecker(cluster, nil)
+	scoreGuard := filter.NewDistinctScoreFilter(s.GetName(), cluster.GetLocationLabels(), stores, source)
+	hitsFilter := s.hitsCounter.buildTargetFilter(s.GetName(), cluster, source)
+	checker := checker.NewReplicaChecker(cluster, nil, s.GetName())
 	storeID, _ := checker.SelectBestReplacementStore(region, oldPeer, scoreGuard, hitsFilter)
 	if storeID == 0 {
 		schedulerCounter.WithLabelValues(s.GetName(), "no-replacement").Inc()
@@ -234,7 +234,7 @@ func (s *balanceRegionScheduler) transferPeer(cluster schedule.Cluster, region *
 	targetLabel := strconv.FormatUint(targetID, 10)
 	s.counter.WithLabelValues("move-peer", source.GetAddress()+"-out", sourceLabel).Inc()
 	s.counter.WithLabelValues("move-peer", target.GetAddress()+"-in", targetLabel).Inc()
-	balanceDirectionCounter.WithLabelValues(s.GetName(), sourceLabel, targetLabel)
+	balanceDirectionCounter.WithLabelValues(s.GetName(), sourceLabel, targetLabel).Inc()
 	return op
 }
 
@@ -309,8 +309,8 @@ func (h *hitsStoreBuilder) put(source, target *core.StoreInfo) {
 	}
 }
 
-func (h *hitsStoreBuilder) buildSourceFilter(cluster schedule.Cluster) filter.Filter {
-	f := filter.NewBlacklistStoreFilter(filter.BlacklistSource)
+func (h *hitsStoreBuilder) buildSourceFilter(scope string, cluster schedule.Cluster) filter.Filter {
+	f := filter.NewBlacklistStoreFilter(scope, filter.BlacklistSource)
 	for _, source := range cluster.GetStores() {
 		if h.filter(source, nil) {
 			f.Add(source.GetID())
@@ -319,8 +319,8 @@ func (h *hitsStoreBuilder) buildSourceFilter(cluster schedule.Cluster) filter.Fi
 	return f
 }
 
-func (h *hitsStoreBuilder) buildTargetFilter(cluster schedule.Cluster, source *core.StoreInfo) filter.Filter {
-	f := filter.NewBlacklistStoreFilter(filter.BlacklistTarget)
+func (h *hitsStoreBuilder) buildTargetFilter(scope string, cluster schedule.Cluster, source *core.StoreInfo) filter.Filter {
+	f := filter.NewBlacklistStoreFilter(scope, filter.BlacklistTarget)
 	for _, target := range cluster.GetStores() {
 		if h.filter(source, target) {
 			f.Add(target.GetID())

server/schedulers/evict_leader.go
@@ -48,13 +48,14 @@ type evictLeaderScheduler struct {
 // newEvictLeaderScheduler creates an admin scheduler that transfers all leaders
 // out of a store.
 func newEvictLeaderScheduler(opController *schedule.OperatorController, storeID uint64) schedule.Scheduler {
+	name := fmt.Sprintf("evict-leader-scheduler-%d", storeID)
 	filters := []filter.Filter{
-		filter.StoreStateFilter{TransferLeader: true},
+		filter.StoreStateFilter{ActionScope: name, TransferLeader: true},
 	}
 	base := newBaseScheduler(opController)
 	return &evictLeaderScheduler{
 		baseScheduler: base,
-		name:          fmt.Sprintf("evict-leader-scheduler-%d", storeID),
+		name:          name,
 		storeID:       storeID,
 		selector:      selector.NewRandomSelector(filters),
 	}

server/schedulers/hot_region.go
@@ -46,6 +46,7 @@ const (
 	hotRegionLimitFactor      = 0.75
 	storeHotRegionsDefaultLen = 100
 	hotRegionScheduleFactor   = 0.9
+	balanceHotRegionName      = "balance-hot-region-scheduler"
 )
 
 // BalanceType : the perspective of balance
@@ -71,6 +72,7 @@ func newStoreStaticstics() *storeStatistics {
 }
 
 type balanceHotRegionsScheduler struct {
+	name string
 	*baseScheduler
 	sync.RWMutex
 	leaderLimit uint64
@@ -85,6 +87,7 @@ type balanceHotRegionsScheduler struct {
 func newBalanceHotRegionsScheduler(opController *schedule.OperatorController) *balanceHotRegionsScheduler {
 	base := newBaseScheduler(opController)
 	return &balanceHotRegionsScheduler{
+		name:          balanceHotRegionName,
 		baseScheduler: base,
 		leaderLimit:   1,
 		peerLimit:     1,
@@ -119,7 +122,7 @@ func newBalanceHotWriteRegionsScheduler(opController *schedule.OperatorControlle
 }
 
 func (h *balanceHotRegionsScheduler) GetName() string {
-	return "balance-hot-region-scheduler"
+	return h.name
 }
 
 func (h *balanceHotRegionsScheduler) GetType() string {
@@ -305,9 +308,9 @@ func (h *balanceHotRegionsScheduler) balanceByPeer(cluster schedule.Cluster, sto
 			log.Error("failed to get the source store", zap.Uint64("store-id", srcStoreID))
 		}
 		filters := []filter.Filter{
-			filter.StoreStateFilter{MoveRegion: true},
-			filter.NewExcludedFilter(srcRegion.GetStoreIds(), srcRegion.GetStoreIds()),
-			filter.NewDistinctScoreFilter(cluster.GetLocationLabels(), cluster.GetRegionStores(srcRegion), srcStore),
+			filter.StoreStateFilter{ActionScope: h.GetName(), MoveRegion: true},
+			filter.NewExcludedFilter(h.GetName(), srcRegion.GetStoreIds(), srcRegion.GetStoreIds()),
+			filter.NewDistinctScoreFilter(h.GetName(), cluster.GetLocationLabels(), cluster.GetRegionStores(srcRegion), srcStore),
 		}
 		candidateStoreIDs := make([]uint64, 0, len(stores))
 		for _, store := range stores {
@@ -366,7 +369,7 @@ func (h *balanceHotRegionsScheduler) balanceByLeader(cluster schedule.Cluster, s
 			continue
 		}
 
-		filters := []filter.Filter{filter.StoreStateFilter{TransferLeader: true}}
+		filters := []filter.Filter{filter.StoreStateFilter{ActionScope: h.GetName(), TransferLeader: true}}
 		candidateStoreIDs := make([]uint64, 0, len(srcRegion.GetPeers())-1)
 		for _, store := range cluster.GetFollowerStores(srcRegion) {
 			if !filter.Target(cluster, store, filters) {

server/schedulers/random_merge.go
@@ -29,7 +29,10 @@ func init() {
 	})
 }
 
+const randomMergeName = "random-merge-scheduler"
+
 type randomMergeScheduler struct {
+	name string
 	*baseScheduler
 	selector *selector.RandomSelector
 }
@@ -38,17 +41,18 @@ type randomMergeScheduler struct {
 // then merges them.
 func newRandomMergeScheduler(opController *schedule.OperatorController) schedule.Scheduler {
 	filters := []filter.Filter{
-		filter.StoreStateFilter{MoveRegion: true},
+		filter.StoreStateFilter{ActionScope: randomMergeName, MoveRegion: true},
 	}
 	base := newBaseScheduler(opController)
 	return &randomMergeScheduler{
+		name:          randomMergeName,
 		baseScheduler: base,
 		selector:      selector.NewRandomSelector(filters),
 	}
 }
 
 func (s *randomMergeScheduler) GetName() string {
-	return "random-merge-scheduler"
+	return s.name
 }
 
 func (s *randomMergeScheduler) GetType() string {

server/schedulers/shuffle_leader.go
@@ -27,7 +27,10 @@ func init() {
 	})
 }
 
+const shuffleLeaderName = "shuffle-leader-scheduler"
+
 type shuffleLeaderScheduler struct {
+	name string
 	*baseScheduler
 	selector *selector.RandomSelector
 }
@@ -36,17 +39,18 @@ type shuffleLeaderScheduler struct {
 // between stores.
 func newShuffleLeaderScheduler(opController *schedule.OperatorController) schedule.Scheduler {
 	filters := []filter.Filter{
-		filter.StoreStateFilter{TransferLeader: true},
+		filter.StoreStateFilter{ActionScope: shuffleLeaderName, TransferLeader: true},
 	}
 	base := newBaseScheduler(opController)
 	return &shuffleLeaderScheduler{
+		name:          shuffleLeaderName,
 		baseScheduler: base,
 		selector:      selector.NewRandomSelector(filters),
 	}
 }
 
 func (s *shuffleLeaderScheduler) GetName() string {
-	return "shuffle-leader-scheduler"
+	return s.name
 }
 
 func (s *shuffleLeaderScheduler) GetType() string {

server/schedulers/shuffle_region.go
@@ -30,7 +30,10 @@ func init() {
 	})
 }
 
+const shuffleRegionName = "shuffle-region-scheduler"
+
 type shuffleRegionScheduler struct {
+	name string
 	*baseScheduler
 	selector *selector.RandomSelector
 }
@@ -39,17 +42,18 @@ type shuffleRegionScheduler struct {
 // between stores.
 func newShuffleRegionScheduler(opController *schedule.OperatorController) schedule.Scheduler {
 	filters := []filter.Filter{
-		filter.StoreStateFilter{MoveRegion: true},
+		filter.StoreStateFilter{ActionScope: shuffleRegionName, MoveRegion: true},
 	}
 	base := newBaseScheduler(opController)
 	return &shuffleRegionScheduler{
+		name:          shuffleRegionName,
 		baseScheduler: base,
 		selector:      selector.NewRandomSelector(filters),
 	}
 }
 
 func (s *shuffleRegionScheduler) GetName() string {
-	return "shuffle-region-scheduler"
+	return s.name
 }
 
 func (s *shuffleRegionScheduler) GetType() string {
@@ -68,7 +72,7 @@ func (s *shuffleRegionScheduler) Schedule(cluster schedule.Cluster) []*operator.
 		return nil
 	}
 
-	excludedFilter := filter.NewExcludedFilter(nil, region.GetStoreIds())
+	excludedFilter := filter.NewExcludedFilter(s.GetName(), nil, region.GetStoreIds())
 	newPeer := s.scheduleAddPeer(cluster, excludedFilter)
 	if newPeer == nil {
 		schedulerCounter.WithLabelValues(s.GetName(), "no-new-peer").Inc()

xing0821/pd

server/schedulers/balance_leader.go
@@ -46,15 +46,10 @@ type balanceLeaderScheduler struct {
 // each store balanced.
 func newBalanceLeaderScheduler(opController *schedule.OperatorController, opts ...BalanceLeaderCreateOption) schedule.Scheduler {
 	taintStores := newTaintCache()
-	filters := []schedule.Filter{
-		schedule.StoreStateFilter{TransferLeader: true},
-		schedule.NewCacheFilter(taintStores),
-	}
 	base := newBaseScheduler(opController)
 
 	s := &balanceLeaderScheduler{
 		baseScheduler: base,
-		selector:      schedule.NewBalanceSelector(core.LeaderKind, filters),
 		taintStores:   taintStores,
 		opController:  opController,
 		counter:       balanceLeaderCounter,
@@ -62,6 +57,11 @@ func newBalanceLeaderScheduler(opController *schedule.OperatorController, opts .
 	for _, opt := range opts {
 		opt(s)
 	}
+	filters := []schedule.Filter{
+		schedule.StoreStateFilter{ActionScope: s.GetName(), TransferLeader: true},
+		schedule.NewCacheFilter(s.GetName(), taintStores),
+	}
+	s.selector = schedule.NewBalanceSelector(core.LeaderKind, filters)
 	return s
 }
 
@@ -218,8 +218,9 @@ func (l *balanceLeaderScheduler) createOperator(region *core.RegionInfo, source,
 	schedulerCounter.WithLabelValues(l.GetName(), "new_operator").Inc()
 	sourceLabel := strconv.FormatUint(sourceID, 10)
 	targetLabel := strconv.FormatUint(targetID, 10)
-	l.counter.WithLabelValues("move_leader", source.GetAddress()+"-out", sourceLabel).Inc()
-	l.counter.WithLabelValues("move_leader", target.GetAddress()+"-in", targetLabel).Inc()
+	l.counter.WithLabelValues("move-leader", source.GetAddress()+"-out", sourceLabel).Inc()
+	l.counter.WithLabelValues("move-leader", target.GetAddress()+"-in", targetLabel).Inc()
+	balanceDirectionCounter.WithLabelValues(l.GetName(), sourceLabel, targetLabel).Inc()
 	op := schedule.CreateTransferLeaderOperator("balance-leader", region, region.GetLeader().GetStoreId(), targetID, schedule.OpBalance)
 	return []*schedule.Operator{op}
 }

server/schedulers/balance_region.go
@@ -47,21 +47,21 @@ type balanceRegionScheduler struct {
 // each store balanced.
 func newBalanceRegionScheduler(opController *schedule.OperatorController, opts ...BalanceRegionCreateOption) schedule.Scheduler {
 	taintStores := newTaintCache()
-	filters := []schedule.Filter{
-		schedule.StoreStateFilter{MoveRegion: true},
-		schedule.NewCacheFilter(taintStores),
-	}
 	base := newBaseScheduler(opController)
 	s := &balanceRegionScheduler{
 		baseScheduler: base,
-		selector:      schedule.NewBalanceSelector(core.RegionKind, filters),
-		taintStores:   taintStores,
 		opController:  opController,
 		counter:       balanceRegionCounter,
+		taintStores:   taintStores,
 	}
 	for _, opt := range opts {
 		opt(s)
 	}
+	filters := []schedule.Filter{
+		schedule.StoreStateFilter{ActionScope: s.GetName(), MoveRegion: true},
+		schedule.NewCacheFilter(s.GetName(), taintStores),
+	}
+	s.selector = schedule.NewBalanceSelector(core.RegionKind, filters)
 	return s
 }
 
@@ -179,9 +179,9 @@ func (s *balanceRegionScheduler) transferPeer(cluster schedule.Cluster, region *
 	// scoreGuard guarantees that the distinct score will not decrease.
 	stores := cluster.GetRegionStores(region)
 	source := cluster.GetStore(oldPeer.GetStoreId())
-	scoreGuard := schedule.NewDistinctScoreFilter(cluster.GetLocationLabels(), stores, source)
+	scoreGuard := schedule.NewDistinctScoreFilter(s.GetName(), cluster.GetLocationLabels(), stores, source)
 
-	checker := schedule.NewReplicaChecker(cluster, nil)
+	checker := schedule.NewReplicaChecker(cluster, nil, s.GetName())
 	storeID, _ := checker.SelectBestReplacementStore(region, oldPeer, scoreGuard)
 	if storeID == 0 {
 		schedulerCounter.WithLabelValues(s.GetName(), "no_replacement").Inc()
@@ -218,9 +218,9 @@ func (s *balanceRegionScheduler) transferPeer(cluster schedule.Cluster, region *
 	}
 	sourceLabel := strconv.FormatUint(sourceID, 10)
 	targetLabel := strconv.FormatUint(targetID, 10)
-	s.counter.WithLabelValues("move_peer", source.GetAddress()+"-out", sourceLabel).Inc()
-	s.counter.WithLabelValues("move_peer", target.GetAddress()+"-in", targetLabel).Inc()
-	s.counter.WithLabelValues("direction", "from_to", sourceLabel+"-"+targetLabel).Inc()
+	s.counter.WithLabelValues("move-peer", source.GetAddress()+"-out", sourceLabel).Inc()
+	s.counter.WithLabelValues("move-peer", target.GetAddress()+"-in", targetLabel).Inc()
+	balanceDirectionCounter.WithLabelValues(s.GetName(), sourceLabel, targetLabel).Inc()
 	return op
 }
 
@@ -231,8 +231,8 @@ func (s *balanceRegionScheduler) transferPeer(cluster schedule.Cluster, region *
 // which may recover soon.
 func (s *balanceRegionScheduler) hasPotentialTarget(cluster schedule.Cluster, region *core.RegionInfo, source *core.StoreInfo, opInfluence schedule.OpInfluence) bool {
 	filters := []schedule.Filter{
-		schedule.NewExcludedFilter(nil, region.GetStoreIds()),
-		schedule.NewDistinctScoreFilter(cluster.GetLocationLabels(), cluster.GetRegionStores(region), source),
+		schedule.NewExcludedFilter(s.GetName(), nil, region.GetStoreIds()),
+		schedule.NewDistinctScoreFilter(s.GetName(), cluster.GetLocationLabels(), cluster.GetRegionStores(region), source),
 	}
 
 	for _, store := range cluster.GetStores() {

server/schedulers/evict_leader.go
@@ -45,13 +45,14 @@ type evictLeaderScheduler struct {
 // newEvictLeaderScheduler creates an admin scheduler that transfers all leaders
 // out of a store.
 func newEvictLeaderScheduler(opController *schedule.OperatorController, storeID uint64) schedule.Scheduler {
+	name := fmt.Sprintf("evict-leader-scheduler-%d", storeID)
 	filters := []schedule.Filter{
-		schedule.StoreStateFilter{TransferLeader: true},
+		schedule.StoreStateFilter{ActionScope: name, TransferLeader: true},
 	}
 	base := newBaseScheduler(opController)
 	return &evictLeaderScheduler{
 		baseScheduler: base,
-		name:          fmt.Sprintf("evict-leader-scheduler-%d", storeID),
+		name:          name,
 		storeID:       storeID,
 		selector:      schedule.NewRandomSelector(filters),
 	}

server/schedulers/hot_region.go
@@ -44,6 +44,7 @@ const (
 	hotRegionLimitFactor      = 0.75
 	storeHotRegionsDefaultLen = 100
 	hotRegionScheduleFactor   = 0.9
+	balanceHotRegionName      = "balance-hot-region-scheduler"
 )
 
 // BalanceType : the perspective of balance
@@ -69,6 +70,7 @@ func newStoreStaticstics() *storeStatistics {
 }
 
 type balanceHotRegionsScheduler struct {
+	name string
 	*baseScheduler
 	sync.RWMutex
 	leaderLimit uint64
@@ -83,6 +85,7 @@ type balanceHotRegionsScheduler struct {
 func newBalanceHotRegionsScheduler(opController *schedule.OperatorController) *balanceHotRegionsScheduler {
 	base := newBaseScheduler(opController)
 	return &balanceHotRegionsScheduler{
+		name:          balanceHotRegionName,
 		baseScheduler: base,
 		leaderLimit:   1,
 		peerLimit:     1,
@@ -117,7 +120,7 @@ func newBalanceHotWriteRegionsScheduler(opController *schedule.OperatorControlle
 }
 
 func (h *balanceHotRegionsScheduler) GetName() string {
-	return "balance-hot-region-scheduler"
+	return h.name
 }
 
 func (h *balanceHotRegionsScheduler) GetType() string {
@@ -305,10 +308,13 @@ func (h *balanceHotRegionsScheduler) balanceByPeer(cluster schedule.Cluster, sto
 		}
 
 		srcStore := cluster.GetStore(srcStoreID)
+		if srcStore == nil {
+			log.Error("failed to get the source store", zap.Uint64("store-id", srcStoreID))
+		}
 		filters := []schedule.Filter{
-			schedule.StoreStateFilter{MoveRegion: true},
-			schedule.NewExcludedFilter(srcRegion.GetStoreIds(), srcRegion.GetStoreIds()),
-			schedule.NewDistinctScoreFilter(cluster.GetLocationLabels(), cluster.GetRegionStores(srcRegion), srcStore),
+			schedule.StoreStateFilter{ActionScope: h.GetName(), MoveRegion: true},
+			schedule.NewExcludedFilter(h.GetName(), srcRegion.GetStoreIds(), srcRegion.GetStoreIds()),
+			schedule.NewDistinctScoreFilter(h.GetName(), cluster.GetLocationLabels(), cluster.GetRegionStores(srcRegion), srcStore),
 		}
 		candidateStoreIDs := make([]uint64, 0, len(stores))
 		for _, store := range stores {
@@ -367,7 +373,7 @@ func (h *balanceHotRegionsScheduler) balanceByLeader(cluster schedule.Cluster, s
 			continue
 		}
 
-		filters := []schedule.Filter{schedule.StoreStateFilter{TransferLeader: true}}
+		filters := []schedule.Filter{schedule.StoreStateFilter{ActionScope: h.GetName(), TransferLeader: true}}
 		candidateStoreIDs := make([]uint64, 0, len(srcRegion.GetPeers())-1)
 		for _, store := range cluster.GetFollowerStores(srcRegion) {
 			if !schedule.FilterTarget(cluster, store, filters) {

server/schedulers/random_merge.go
@@ -26,7 +26,10 @@ func init() {
 	})
 }
 
+const randomMergeName = "random-merge-scheduler"
+
 type randomMergeScheduler struct {
+	name string
 	*baseScheduler
 	selector *schedule.RandomSelector
 }
@@ -35,17 +38,18 @@ type randomMergeScheduler struct {
 // then merges them.
 func newRandomMergeScheduler(opController *schedule.OperatorController) schedule.Scheduler {
 	filters := []schedule.Filter{
-		schedule.StoreStateFilter{MoveRegion: true},
+		schedule.StoreStateFilter{ActionScope: randomMergeName, MoveRegion: true},
 	}
 	base := newBaseScheduler(opController)
 	return &randomMergeScheduler{
+		name:          randomMergeName,
 		baseScheduler: base,
 		selector:      schedule.NewRandomSelector(filters),
 	}
 }
 
 func (s *randomMergeScheduler) GetName() string {
-	return "random-merge-scheduler"
+	return s.name
 }
 
 func (s *randomMergeScheduler) GetType() string {

server/schedulers/shuffle_leader.go
@@ -24,7 +24,10 @@ func init() {
 	})
 }
 
+const shuffleLeaderName = "shuffle-leader-scheduler"
+
 type shuffleLeaderScheduler struct {
+	name string
 	*baseScheduler
 	selector *schedule.RandomSelector
 }
@@ -33,17 +36,18 @@ type shuffleLeaderScheduler struct {
 // between stores.
 func newShuffleLeaderScheduler(opController *schedule.OperatorController) schedule.Scheduler {
 	filters := []schedule.Filter{
-		schedule.StoreStateFilter{TransferLeader: true},
+		schedule.StoreStateFilter{ActionScope: shuffleLeaderName, TransferLeader: true},
 	}
 	base := newBaseScheduler(opController)
 	return &shuffleLeaderScheduler{
+		name:          shuffleLeaderName,
 		baseScheduler: base,
 		selector:      schedule.NewRandomSelector(filters),
 	}
 }
 
 func (s *shuffleLeaderScheduler) GetName() string {
-	return "shuffle-leader-scheduler"
+	return s.name
 }
 
 func (s *shuffleLeaderScheduler) GetType() string {

server/schedulers/shuffle_region.go
@@ -27,7 +27,10 @@ func init() {
 	})
 }
 
+const shuffleRegionName = "shuffle-region-scheduler"
+
 type shuffleRegionScheduler struct {
+	name string
 	*baseScheduler
 	selector *schedule.RandomSelector
 }
@@ -36,17 +39,18 @@ type shuffleRegionScheduler struct {
 // between stores.
 func newShuffleRegionScheduler(opController *schedule.OperatorController) schedule.Scheduler {
 	filters := []schedule.Filter{
-		schedule.StoreStateFilter{MoveRegion: true},
+		schedule.StoreStateFilter{ActionScope: shuffleRegionName, MoveRegion: true},
 	}
 	base := newBaseScheduler(opController)
 	return &shuffleRegionScheduler{
+		name:          shuffleRegionName,
 		baseScheduler: base,
 		selector:      schedule.NewRandomSelector(filters),
 	}
 }
 
 func (s *shuffleRegionScheduler) GetName() string {
-	return "shuffle-region-scheduler"
+	return s.name
 }
 
 func (s *shuffleRegionScheduler) GetType() string {
@@ -65,7 +69,7 @@ func (s *shuffleRegionScheduler) Schedule(cluster schedule.Cluster) []*schedule.
 		return nil
 	}
 
-	excludedFilter := schedule.NewExcludedFilter(nil, region.GetStoreIds())
+	excludedFilter := schedule.NewExcludedFilter(s.GetName(), nil, region.GetStoreIds())
 	newPeer := s.scheduleAddPeer(cluster, excludedFilter)
 	if newPeer == nil {
 		schedulerCounter.WithLabelValues(s.GetName(), "no_new_peer").Inc()