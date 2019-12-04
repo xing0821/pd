31946ccc3a0aaf7dab9d31cf595c26f6472eeea7
Ryan Leung <rleungx@gmail.com>
2019-12-04 11:38:41 +0800

    address the comment
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/schedule/operator_controller.go
+++ server/schedule/operator_controller.go
@@ -270,7 +270,6 @@ func (oc *OperatorController) AddWaitingOperator(ops ...*operator.Operator) bool
 			oc.buryOperator(op)
 		}
 		oc.Unlock()
-		fmt.Println("xxxx1")
 		return false
 	}
 	oc.wop.PutOperator(op)
